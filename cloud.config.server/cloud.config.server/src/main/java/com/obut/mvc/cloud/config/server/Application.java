3202fe824e9c9e92df26f459be9260018927d9b1
AhmetObut56 <129781646+AhmetObut56@users.noreply.github.com>
2023-04-03 21:42:41 +0300

    configserverupload3
    configserver ile ilgili çalışma

---
+++ cloud.config.server/cloud.config.server/src/main/java/com/obut/mvc/cloud/config/server/Application.java
@@ -0,0 +1,13 @@
+package com.obut.mvc.cloud.config.server;
+//properties 2.adımda adlandırma yaptı
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
